82fbc4e4fb4e2dc1369275abe4feb0e94951707c
Ryon Coleman <ryon55@gmail.com>
2021-06-23 15:46:25 +0000

    feat: add homepage; refactor templates; move example report

--- generate.py
+++ generate.py
@@ -4,27 +4,45 @@ import re
 from datetime import date
 from glob import glob
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
 env = Environment(
-    loader=FileSystemLoader("templates"),
+    loader=FileSystemLoader('templates'),
     autoescape=select_autoescape()
 )
 
-template = env.get_template("index.html")
+################################################################################
+# render index
 
-template_data = {
-    "date_generated": date.today()
+index_template = env.get_template('index.html.jinja')
+
+index_data = {}
+with open('data/index_report.json') as file:
+    index_data['reports'] = json.load(file)
+
+index_html = index_template.render(index_data)
+
+if not os.path.exists('build'):
+    os.mkdir('build')
+with open('build/index.html', 'w') as file:
+    file.write(index_html)
+
+################################################################################
+# render individual report for 2021/07
+# TODO make this process dynamic; remove hardcoded dates
+
+report_template = env.get_template('report.html.jinja')
+
+report_data = {
+    'date_generated': date.today()
 }
-for json_file in glob('data/*.json'):
+for json_file in glob('data/2021/07/*.json'):
     name = re.sub(r'^\d+_(.+).json$', r'\1', os.path.basename(json_file))
-    with open(json_file, "r") as file:
-        template_data[name] = json.load(file)
+    with open(json_file, 'r') as file:
+        report_data[name] = json.load(file)
 
-# with open("data/1_feed_info.json", "r") as file:
-#     feed_info = json.load(file)
+report_html = report_template.render(report_data)
 
-output_html = template.render(template_data)
-
-if not os.path.exists("build"):
-    os.mkdir("build")
-with open("build/index.html", "w") as file:
-    file.write(output_html)
+if not os.path.exists('build/2021/07'):
+    os.makedirs('build/2021/07')
+with open('build/2021/07/index.html', 'w') as file:
+    file.write(report_html)
