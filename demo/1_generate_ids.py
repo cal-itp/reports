3b7d06ee49a66d1436dfa9340a019902a4d066d0
Michael Chow <mc_al_github@fastmail.com>
2021-06-23 12:44:57 -0400

    generate index for reports for homepage

--- demo/1_generate_ids.py
+++ demo/1_generate_ids.py
@@ -33,15 +33,33 @@ tbl_status
 ids_with_feeds = (
     tbl_status
     >> filter(_.url_number==0)
-    >> distinct(calitp_itp_id=_.itp_id, calitp_url_number=_.url_number)
+    >> rename(calitp_itp_id=_.itp_id, calitp_url_number=_.url_number)
     >> collect()
     >> inner_join(_, feeds, ["calitp_itp_id", "calitp_url_number"])
     >> arrange(_.calitp_itp_id, _.calitp_url_number)
 )
 
-ids_with_feeds >> count()
+# +
+# generate an index for the homepage
+from collections import defaultdict
 
-build_report(45)
+df_report_index = (
+    ids_with_feeds
+    >> select(-_.status, -_.calitp_extracted_at, -_.calitp_url_number, -_.gtfs_schedule_url)
+    >> mutate(year=2021, month=5, report_path=_.apply(lambda d: f"{d.year}/{d.month:02d}/{d.calitp_itp_id}.html", axis=1))
+    #>> pipe(_.to_json("index_report.json", orient="records"))
+)
+
+cols_to_keep = ["agency_name", "calitp_itp_id", "report_path"]
+report_index = defaultdict(lambda: defaultdict(lambda: {}))
+for (year, month), g in df_report_index.groupby(["year", "month"]):
+    # note that year and month are originally numpy.int64...
+    report_index[int(year)][f"{month:02d}"] = g[cols_to_keep].to_dict(orient="records")
+    
+# -
+
+import json
+json.dump(report_index, open("report_index.json", "w"))
 
 # +
 from papermill.exceptions import PapermillExecutionError
