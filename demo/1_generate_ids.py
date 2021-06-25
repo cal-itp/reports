d863ac7ca565cddda9aa4eef5a4cd0757adfbb91
Michael Chow <mc_al_github@fastmail.com>
2021-06-25 12:52:30 -0400

    update report scripts

--- demo/1_generate_ids.py
+++ demo/1_generate_ids.py
@@ -45,39 +45,41 @@ from collections import defaultdict
 
 df_report_index = (
     ids_with_feeds
-    >> select(-_.status, -_.calitp_extracted_at, -_.calitp_url_number, -_.gtfs_schedule_url)
-    >> mutate(year=2021, month=5, report_path=_.apply(lambda d: f"{d.year}/{d.month:02d}/{d.calitp_itp_id}.html", axis=1))
-    #>> pipe(_.to_json("index_report.json", orient="records"))
+    >> select(
+        -_.status, -_.calitp_extracted_at, -_.calitp_url_number, -_.gtfs_schedule_url
+    )
+    >> mutate(
+        year=2021,
+        month=5,
+        path=_.apply(
+            lambda d: f"{d.year}/{d.month:02d}/{d.calitp_itp_id}.html", axis=1
+        ),
+    )
+    # >> pipe(_.to_json("index_report.json", orient="records"))
 )
 
-cols_to_keep = ["agency_name", "calitp_itp_id", "report_path"]
-report_index = defaultdict(lambda: defaultdict(lambda: {}))
-for (year, month), g in df_report_index.groupby(["year", "month"]):
-    # note that year and month are originally numpy.int64...
-    report_index[int(year)][f"{month:02d}"] = g[cols_to_keep].to_dict(orient="records")
-    
+cols_to_keep = ["agency_name", "itp_id", "path"]
+
+index_report = (
+    df_report_index
+    >> rename(itp_id = _.calitp_itp_id)
+    >> arrange(_.year, _.month)
+    >> group_by(_.year, _.month)
+    >> summarize(reports=lambda _: [_[cols_to_keep].to_dict(orient="records")])
+    >> group_by(_.year)
+    >> summarize(
+        first_month=_.month.min(),
+        last_month=_.month.max(),
+        months=lambda _: [_[["month", "reports"]].to_dict(orient="records")],
+    )
+)
+
+index_report.to_json("index_report.json", orient="records")
+# report_index = defaultdict(lambda: defaultdict(lambda: {}))
+# for (year, month), g in df_report_index.groupby(["year", "month"]):
+#     # note that year and month are originally numpy.int64...
+#     report_index[int(year)][f"{month:02d}"] = g[cols_to_keep].to_dict(orient="records")
 # -
 
-import json
-json.dump(report_index, open("report_index.json", "w"))
-
-# +
-from papermill.exceptions import PapermillExecutionError
-
-from concurrent.futures import ThreadPoolExecutor
-
-
-def build_report(itp_id):
-    p_dir = Path(f"output/{itp_id}")
-    p_dir.mkdir(parents=True, exist_ok=True)
-    
-    fname = str(p_dir / "report.ipynb")
-    
-    print(f"Knitting to: {fname}")
-
-    pm.execute_notebook("report.ipynb", fname, parameters={"CALITP_ITP_ID": itp_id, "CALITP_URL_NUMBER": 0})
-
 all_ids = list(ids_with_feeds.calitp_itp_id)
-
-with ThreadPoolExecutor(max_workers=8) as executor:
-    executor.map(build_report, all_ids)
+" ".join(map(str,all_ids))
