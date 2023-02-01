6006542a073c9caea4cd38e46004107555fec723
Aaron Couch <acinternets@gmail.com>
2023-02-01 14:06:42 -0500

    fix: add file_check

--- reports/generate_reports.py
+++ reports/generate_reports.py
@@ -4,7 +4,8 @@ import json
 import warnings
 from calitp.tables import tbl
 from datetime import date, datetime, timedelta
-from siuba import filter as filtr, _, collect, pipe, select, rename, mutate
+import pandas as pd
+from siuba import filter as filtr, _, collect, pipe, select, rename, mutate, right_join, spread, arrange, if_else
 import argparse
 from pathlib import Path
 import calendar
@@ -28,14 +29,17 @@ date_end = f"{year}-{month}-{last_day_month}"
 if not args.v:
   warnings.filterwarnings("ignore")
 
-index = {}
-with open('../reports/outputs/index_report.json') as file:
-    index = json.load(file)
-
-year_index = list(filter(lambda x: (x['year'] == year), index))[0]
-month_index = list(filter(lambda x: (x['month'] == month), year_index['months']))[0]
-
 # Functions
+def to_rowspan_table(df, span_col):
+    d = df.to_dict(orient="split")
+    row_span = df.groupby(span_col)[span_col].transform("count")
+    not_first = df[span_col].duplicated()
+    
+    row_span[not_first] = 0
+    
+    d["rowspan"] = row_span.tolist()
+    return d
+
 def generate_service_days(service_days):
   n_days_no_service = {"n": service_days}
   return n_days_no_service
@@ -65,50 +69,85 @@ def generate_daily_service_hours(itp_id, date_start, date_end):
   )
   return service_hours
   
-
-for report in month_index['reports']:
-  # Use cached data.
-  report_data_path = f"../reports/outputs/{report['report_path']}/report.json"
-  out_dir = Path(f"outputs/{report['report_path']}/data")
+def get_param_data(itp_id, month, year):
+  param_data_path = f"../reports/outputs/{year}/{month}/{itp_id}/report.json"
+  out_dir = Path(f"outputs/{year}/{month}/{itp_id}/data")
   out_dir.mkdir(parents=True, exist_ok=True)
 
-  with open(report_data_path) as file:
-    report_data = json.load(file)
+  with open(param_data_path) as file:
+    param_data = json.load(file)
+  return param_data
 
-  # 1_feed_info.json
+def generate_file_check(itp_id, date_start):
 
-  # 2_n_days_no_service.json
-  if args.v:
-    print(f"Generating service days for {report['itp_id']}")
-  service_days = int(report_data['no_service_days_ct']) if report_data['no_service_days_ct'] is not None else None
-  n_days_no_service = generate_service_days(service_days)
-  json.dump(n_days_no_service, open(out_dir / "2_n_days_no_service.json", "w"))  
+  importance = ["Visual display", "Navigation", "Fares", "Technical contacts"]
 
-  # 2_daily_service_hours.json
-  if args.v:
-    print(f"Generating service hours for {report['itp_id']}")
-  service_hours = generate_daily_service_hours(report['itp_id'], date_start, date_end)
-  service_hours.to_json(out_dir / "2_daily_service_hours.json", orient="records")
-
-  # 3_stops_changed.json
-
-  # 3_routes_changed.json
-
-  # 4_file_check.json
-  file_check =(
+  fc =(
     tbl.mart_gtfs_quality.fct_monthly_reports_site_organization_file_checks()
-    >> filtr(_.organization_itp_id == report['itp_id'])
+    >> filtr(_.organization_itp_id == itp_id)
+    >> filtr(_.publish_date == date_start)
+    >> select(_.date_checked, _.reason, _.filename, _.file_present)
     >> collect()
+    >> rename(success = _.file_present)
+    >> rename(name = _.filename)
+    >> rename(category = _.reason)
+    >> mutate(
+      success = if_else(_.success == True, "✅", ""),
+      date_checked = _.date_checked.astype(str),
+    )
+    >> spread(_.date_checked, _.success)
+    >> arrange(_.category.apply(importance.index))
+    >> pipe(_.fillna(""))
   )
-  if args.v:
-    print(f"Generating file check for {report['itp_id']}")  
-  # 4_validation_severity.json
+  return fc
 
-  # 5_validation_notices.json
-  validation_notices = (
-    tbl.fct_monthly_reports_site_organization_validation_codes()
-    >> filtr(_.organization_itp_id == report['itp_id'])
-    >> collect()
-  )
-  if args.v:
-    print(f"Generating validation notices for {report['itp_id']}")  
+def generate_report_data_by_month(month, year, date_start, end_date):
+  index = {}
+  with open('../reports/outputs/index_report.json') as file:
+    index = json.load(file)
+ 
+  year_index = list(filter(lambda x: (x['year'] == year), index))[0]
+  month_index = list(filter(lambda x: (x['month'] == month), year_index['months']))[0]
+  year_index = list(filter(lambda x: (x['year'] == year), index))[0]
+  month_index = list(filter(lambda x: (x['month'] == month), year_index['months']))[0]
+  
+  for report in month_index['reports']:
+    # Use cached data.
+    param_data = get_param_data(report['itp_id'], month, year)
+    out_dir = Path(f"outputs/{year}/{month}/{report['itp_id']}/data")
+
+    # 1_feed_info.json
+
+    # 2_n_days_no_service.json
+    if args.v:
+      print(f"Generating service days for {report['itp_id']}")
+    service_days = int(param_data['no_service_days_ct']) if param_data['no_service_days_ct'] is not None else None
+    n_days_no_service = generate_service_days(service_days)
+    json.dump(n_days_no_service, open(out_dir / "2_n_days_no_service.json", "w"))  
+
+    # 2_daily_service_hours.json
+    if args.v:
+      print(f"Generating service hours for {report['itp_id']}")
+    service_hours = generate_daily_service_hours(report['itp_id'], date_start, date_end)
+    service_hours.to_json(out_dir / "2_daily_service_hours.json", orient="records")
+
+    # 3_stops_changed.json
+
+    # 3_routes_changed.json
+
+    # 4_file_check.json
+    if args.v:
+      print(f"Generating file check for {report['itp_id']}") 
+    file_check = generate_file_check(report['itp_id'], date_start) 
+    json.dump(to_rowspan_table(file_check, "category"), open("./4_file_check.json", "w"))
+
+    # 4_validation_severity.json
+
+    # 5_validation_notices.json
+    validation_notices = (
+      tbl.fct_monthly_reports_site_organization_validation_codes()
+      >> filtr(_.organization_itp_id == report['itp_id'])
+      >> collect()
+    )
+    if args.v:
+      print(f"Generating validation notices for {report['itp_id']}")  
