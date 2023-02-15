fca2ca8d21ace932ffb3577d470056d695baf60f
Aaron Couch <acinternets@gmail.com>
2023-02-15 09:23:50 -0500

    fix: updates test and validation files for linting and style

--- tests/test_report_data.py
+++ tests/test_report_data.py
@@ -1,16 +1,19 @@
-from jsonschema import validate
 import json
 import os
 import sys
-sys.path.append(os.path.abspath('../reports'))
-from generate_reports_data import generate_data_by_file_path 
-from validate_reports import validate_files  
+
+from jsonschema import validate
+
+sys.path.append(os.path.abspath("../reports"))
+from generate_reports_data import generate_data_by_file_path
+from validate_reports import validate_files
+
 test_file = "outputs/2022/11/91/1_file_info.json"
 
 # Instead of unit tests, this generates a test output directory, then validates
 # the data.
-print (f"generating {test_file}")
-#generate_data_by_file_path(test_file)
+print(f"generating {test_file}")
+# generate_data_by_file_path(test_file)
 
 files = [
     "1_feed_info.json",
@@ -18,7 +21,7 @@ files = [
     "3_routes_changed.json",
     "3_stops_changed.json",
     "4_file_check.json",
-    "5_validation_codes.json"
+    "5_validation_codes.json",
 ]
 output_dir = "outputs/2022/11/91"
 schema_dir = "schemas"
